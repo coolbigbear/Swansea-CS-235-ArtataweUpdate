1b7b08130e5fa20928602020d91af21d3d3bc0b8
Bassam Helal <basshelal@gmail.com>
2017-11-20 14:00:44 +0000

    Added TestSuite

---
+++ source/test/TestSuite.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestSuite {
+	
+	@Test
+	void test(){
+	    assertEquals( 2+2, 4);
+	}
+	
+}
