9629bb94123082bee0e4f02dcf7ec03a41209776
Bassam Helal <basshelal@gmail.com>
2017-11-27 22:34:50 +0000

    Finished Bid documentation

--- source/model/Bid.java
+++ source/model/Bid.java
@@ -1,7 +1,29 @@
 package model;
 
+/* TODO: 27-Nov-17 Bassam Helal We should consider using something other than the java.lang.Date
+ * we could use the Calendar class or better yet the javax.time package, I will look into this
+ */
 import java.util.Date;
 
+/**
+ * A Bid is an offer on an Auction with a proposed amount, all made by the current user
+ * at a point in time.
+ *
+ * A Bid cannot exist without these attributes, specifically a Bid requires:
+ * <ul>
+ * <li>Auction which the Bid will be placed on.</li>
+ * <li>Profile which is the current user, or the person placing the Bid</li>
+ * <li>Bid Amount which is the amount associated with the Bid</li>
+ * <li>Date which is the date and time the Bid was placed</li>
+ * </ul>
+ *
+ * Notice that Bid validation on an Auction does not occur here, instead on the Auction,
+ * meaning a Bid instance may exist where it does not satisfy the validation requirements of
+ * the Auction, this Bid will not exist on the Database.
+ *
+ * @author Bassam Helal
+ * @version 1.0
+ */
 public final class Bid {
 	
 	private final Double bidAmount;
@@ -9,6 +31,14 @@ public final class Bid {
 	private final Profile bidder;
 	private final Auction auction;
 	
+	/**
+	 * The primary and only constructor for a Bid, this assumes the Profile
+	 * placing the Bid is the current user.
+	 *
+	 * @param auction the Auction which the Bid will be placed on
+	 * @param bidAmount the Double representing the amount or price associated
+	 * 		with the Bid
+	 */
 	public Bid(Auction auction, Double bidAmount) {
 		this.auction = auction;
 		this.bidAmount = bidAmount;
@@ -16,18 +46,34 @@ public final class Bid {
 		this.bidder = Util.currentUser;
 	}
 	
+	/**
+	 * Gets the Bid amount
+	 * @return the amount or price associated with the Bid
+	 */
 	public Double getBidAmount() {
 		return this.bidAmount;
 	}
 	
+	/**
+	 * Gets the time the Bid was placed
+	 * @return the time the Bid was placed
+	 */
 	public Date getTimePlaced() {
 		return this.timePlaced;
 	}
 	
+	/**
+	 * Gets the Profile that placed the Bid
+	 * @return the Profile that placed the Bid
+	 */
 	public Profile getBidder() {
 		return this.bidder;
 	}
 	
+	/**
+	 * Gets the Auction that the Bid will be placed on
+	 * @return the Auction the Bid will be placed on
+	 */
 	public Auction getAuction() {
 		return this.auction;
 	}
