f914f7b650a3217004a06d9df65625c6f1fa1826
MINIDRAGON-PC\Michal Czuper <mrczuper@gmail.com>
2017-11-28 00:44:31 +0000

    Add very rough reading of a profile json file and Gson library

--- source/model/Util.java
+++ source/model/Util.java
@@ -1,7 +1,31 @@
 package model;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+/**
+ * The type Util.
+ */
 public class Util {
-	
-	
+
+	/**
+	 * The current user who is signed in to the system.
+	 */
 	public static Profile currentUser;
+
+	Gson gson = new Gson();
+
+	public void readCurrentUser() {
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader( new FileReader("profile.json"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		currentUser = gson.fromJson(br,Profile.class);
+	}
 }
