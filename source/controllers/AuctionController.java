902ef3bf67d3ee36da97c2af4d1dc5a19c1aef23
Bezhan Kodomani <shadow_bez@yahoo.com>
2017-12-07 00:08:20 +0000

    AuctionController updated

--- source/controllers/AuctionController.java
+++ source/controllers/AuctionController.java
@@ -1,5 +1,20 @@
 package controllers;
 
-public class AuctionController {
+import javafx.fxml.Initializable;
+import model.Auction;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AuctionController implements Initializable {
+
     //TODO view users to the seller who have placed bids on the auction!!!!!!!!!!!!!!!
+    private Auction currentAuction;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+    public void initAuction(Auction auction) {
+        currentAuction = auction;
+    }
 }
