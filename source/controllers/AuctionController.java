42807cfbde8e73af14ff6a4cbc358f3d6ae7e0af
Bezhan Kodomani <shadow_bez@yahoo.com>
2017-12-07 02:08:01 +0000

    Card layout updated

--- source/controllers/AuctionController.java
+++ source/controllers/AuctionController.java
@@ -5,10 +5,9 @@ import model.Auction;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
+//TODO view users to the seller who have placed bids on the auction!!!!!!!!!!!!!!!
 public class AuctionController implements Initializable {
 
-    //TODO view users to the seller who have placed bids on the auction!!!!!!!!!!!!!!!
     private Auction currentAuction;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
