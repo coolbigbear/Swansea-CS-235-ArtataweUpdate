e3f40a0412e7dcb04ec9573ce59de85dde758a9d
MINIDRAGON-PC\Michal Czuper <mrczuper@gmail.com>
2017-12-08 02:07:48 +0000

    Started work on create auction controller

--- source/controllers/CreateAuctionController.java
+++ source/controllers/CreateAuctionController.java
@@ -1,11 +1,18 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import model.Artwork;
 import model.Auction;
 import model.Util;
 
 public class CreateAuctionController {
-	
+	@FXML
+	private Button sellPaintingButton;
+	private Button sellSculptureButton;
+
 	//All the FXML Views/ Nodes
 	
 	/* Used to create a new Auction and submit it to Database
@@ -19,9 +26,14 @@ public class CreateAuctionController {
 		// if (type is Artwork) { ... }
 		// if (type is Sculpture) { ... }
 		// inside these ifs we'd set the the Artwork constructor params from the TextFields
+		sellPaintingButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override public void handle(ActionEvent e) {
+				//label.setText("Accepted");
+			}
+
 		
 		Auction auction = Auction.createNewAuction(artwork, Util.getCurrentUser().getUsername(), 5, 5.00);
-		Util.saveAuctionToFile(auction); // we can actually delegate this to where the method above is declared in
+		//Util.saveAuctionToFile(auction); // we can actually delegate this to where the method above is declared in
 		// Auction instead of here
 	}
 }
