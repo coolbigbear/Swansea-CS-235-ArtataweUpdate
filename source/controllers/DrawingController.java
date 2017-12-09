35034f929e635ff693d40846ca95b3f165a670e5
Iliyan Garnev <ilian.garnev@gmail.com>
2017-12-09 22:21:34 +0000

    Added some features for the custom image

--- source/controllers/DrawingController.java
+++ source/controllers/DrawingController.java
@@ -2,15 +2,16 @@ package controllers;
 
 
 import javafx.embed.swing.SwingFXUtils;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ColorPicker;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 
 import javax.imageio.ImageIO;
+import java.awt.event.ActionEvent;
 import java.io.File;
 import java.util.UUID;
 
@@ -26,42 +27,73 @@ public class DrawingController {
     private TextField brushSize;
 
     @FXML
-    private CheckBox ovalPaint;
+    private RadioMenuItem freeDraw;
 
-    public void onSave(){
-        try{
+    @FXML
+    private RadioMenuItem drawRectangle;
+
+    @FXML
+    private RadioMenuItem drawCircle;
+
+    @FXML
+    private RadioMenuItem drawLine;
+
+    public void onSave() {
+        try {
             Image snapshot = canvas.snapshot(null, null);
             ImageIO.write(SwingFXUtils.fromFXImage(snapshot, null), "png", new File
                     (generateNameAndSetLocation()));
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Cannot save file!" + e);
         }
         //TODO Set image and Close custom drawing window
+        //TODO ADD TO GSON
     }
 
-    public void onClear(){
+    public void onClear() {
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        gc.clearRect(0,0, canvas.getWidth(), canvas.getHeight());
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
     }
 
-    public void initialize(){
+    public void initialize() {
         GraphicsContext gc = canvas.getGraphicsContext2D();
+
         canvas.setOnMouseDragged(event -> {
-            double size = Double.parseDouble(brushSize.getText());
-            double x = event.getX();
-            double y = event.getY();
-            gc.setFill(colorPicker.getValue());
-            if(ovalPaint.isSelected()) {
-                gc.fillOval(x, y, size/2, size/2);
-            }else{
-                gc.fillRect(x, y, size/2, size/2);
+            if (freeDraw.isSelected()) {
+                double size = Double.parseDouble(brushSize.getText());
+                double x = event.getX();
+                double y = event.getY();
+                gc.setFill(colorPicker.getValue());
+                gc.fillOval(x, y, size / 2, size / 2);
+            } else if (drawLine.isSelected()) { //TODO FIX LINE
+                double x = event.getX();
+                double y = event.getY();
+                gc.setFill(colorPicker.getValue());
+                gc.fillRect(x,y,30,30);
+                double x1 = event.getX();
+                double y1 = event.getY();
+                gc.lineTo(x1,y1);
+            }
+        });
+        canvas.setOnMouseClicked(event -> {
+            if (drawRectangle.isSelected()) {
+                double x = event.getX();
+                double y = event.getY();
+                gc.setFill(colorPicker.getValue());
+                gc.fillRect(x, y, 40, 40);
+            } else if (drawCircle.isSelected()) {
+                double x = event.getX();
+                double y = event.getY();
+                gc.setFill(colorPicker.getValue());
+                gc.fillOval(x, y, 40, 40);
             }
         });
     }
 
+
     private String generateNameAndSetLocation() {
         String generatedString = UUID.randomUUID().toString();
-        return "source/images/custom/" + generatedString +".png";
+        return "source/images/custom/" + generatedString + ".png";
     }
 
 }
