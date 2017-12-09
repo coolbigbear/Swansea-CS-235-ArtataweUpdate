0c8ca9928ba1316b26054d79d25f9564a6203b14
Iliyan Garnev <ilian.garnev@gmail.com>
2017-12-09 15:34:18 +0000

    Added a favourite user button in auction view

--- source/model/Artwork.java
+++ source/model/Artwork.java
@@ -1,15 +1,17 @@
 package model;
 
+import java.time.LocalDate;
+
 // TODO: 04-Dec-17 Documentation!
 public abstract class Artwork {
-	
+
 	protected String title;
 	protected StringBuilder description;
 	protected String creationDate;
 	protected String creatorName;
 	protected String mainImagePath;
 	protected ArtworkType type;
-	
+
 	public Artwork(String title, StringBuilder description, String creationDate, String creatorName, String mainImagePath) {
 		this.title = title;
 		this.description = description;
@@ -17,37 +19,37 @@ public abstract class Artwork {
 		this.creatorName = creatorName;
 		this.mainImagePath = mainImagePath;
 	}
-	
+
 	public abstract ArtworkType getType();
-	
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
+
 	public void setDescription(StringBuilder description) {
 		this.description = description;
 	}
-	
+
 	public void setCreationDate(String creationDate) {
 		this.creationDate = creationDate;
 	}
-	
+
 	public void setCreatorName(String creatorName) {
 		this.creatorName = creatorName;
 	}
-	
+
 	public void setMainImagePath(String mainImagePath) {
 		this.mainImagePath = mainImagePath;
 	}
-	
+
 	public String getTitle() {
 		return this.title;
 	}
-	
+
 	public StringBuilder getDescription() {
 		return this.description;
 	}
-	
+
 	public String getCreationDate() {
 		return this.creationDate;
 	}
