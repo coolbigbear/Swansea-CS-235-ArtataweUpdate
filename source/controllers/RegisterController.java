17d46b10d19fe6e465677da455ad137f7844a6cf
MINIDRAGON-PC\Michal Czuper <mrczuper@gmail.com>
2017-12-09 22:19:32 +0000

    Made login look fxml look nice

--- source/controllers/RegisterController.java
+++ source/controllers/RegisterController.java
@@ -1,4 +1,21 @@
 package controllers;
 
-public class RegisterController {
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegisterController implements Initializable {
+
+    @FXML
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
+
