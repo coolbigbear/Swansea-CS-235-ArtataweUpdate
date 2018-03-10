ae3cfe07beed8e84f5fde756d48d88ffc8c79b68
MINIDRAGON-PC\Michal Czuper <mrczuper@gmail.com>
2018-03-10 15:56:32 +0000

    Search has been implemented fully. Searches through: - name of auction - description - creation date - creator name

--- source/controllers/FeedController.java
+++ source/controllers/FeedController.java
@@ -5,7 +5,10 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -25,6 +28,11 @@ public class FeedController implements Initializable {
 	private GridPane cardsGridPane;
 	@FXML
 	private ChoiceBox choiceBoxFilter;
+	@FXML
+	private TextField searchBar;
+	@FXML
+	private Button searchButton;
+
 	private ObservableList<String> choiceBoxList = FXCollections.observableArrayList("Show All", "Paintings",
 			"Sculptures");
 	private Feed feed;
@@ -46,10 +54,30 @@ public class FeedController implements Initializable {
 		setChoiceBox();
 		Util.setFilterChoiceBox(choiceBoxFilter);
 	}
-	
-	// TODO: 09-Dec-17 Bassam please fix this thing!!!
-	private void setChoiceBox() {
 
+	public void searchButtonPress () throws IOException {
+		search(searchBar.getText());
+	}
+
+	public void search(String searchQuery) throws IOException {
+		Util.getAuctionsByName(searchQuery);
+		setAuctionsCenter();
+	}
+
+	@FXML
+	public void onEnter() {
+		searchBar.setOnKeyPressed(event -> {
+            if (event.getCode().equals(KeyCode.ENTER)) {
+				try {
+					search(searchBar.getText());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+        });
+	}
+
+	private void setChoiceBox() {
 		choiceBoxFilter.getSelectionModel().selectedIndexProperty().addListener(
 				(observable, oldValue, newValue) -> {
 					try {
